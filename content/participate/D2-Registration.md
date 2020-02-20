1f38aff4c57835764ccbeb0863822981436eb928
silverlak3 <freefastfire@gmail.com>
2020-02-20 00:24:22 -0800

    Rafa Commit

--- content/participate/D2-Registration.md
+++ content/participate/D2-Registration.md
@@ -3,16 +3,9 @@ title: "Division 2 Registration"
 date: 2020-02-19T23:48:07-08:00
 draft: true
 ---
+This is the registration page for Division 2 teams.
 <!--more-->
-<div id="bbox-root">
-test
-</div>
-<script type="text/javascript">
-  print "hello";
-  window.bboxInit = function () { bbox.showForm('4f0daac2-91a5-4f5e-9fcf-937f95dfe1fb'); };
-  (function () {
-    var e = document.createElement('script'); e.async = true;
-    e.src = 'https://bbox.blackbaudhosting.com/webforms/bbox-min.js';
-    document.getElementsByTagName('head')[0].appendChild(e);
-    } ());
-</script>
+*Please fill out the form below. If the form does not render, make sure javascript is enabled.*
+
+<script src="/js/d2registration.js"></script>
+<div id="bbox-root"></div>
---
+++ rafa-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# THIS IS RAFA'S PERSONAL COMMIT FILE.
+REPO=~/Documents/GitHub/wrccdc.github.io/
+
+# Build the site.
+echo "-------------------------------"
+echo -e "\033[0;32mCompiling site...\033[0m"
+echo "-------------------------------"
+hugo -t ananke # if using a theme, replace with `hugo -t <YOURTHEME>` # this creates the "public" folder which is the final website.
+
+cp -r public/ $REPO
+
+# cp -r public [compiled hugo website.io repo location]
+# Replace the location above to where ever you locally cloned the second repo to.
+# This is basically where the finished website lives and gets finally pushed up to github.
+
+echo "-------------------------------"
+echo "Compiled Site Location: $REPO "
+echo -e "\033[0;32mDONE... Don't forget to Commit and Push!\033[0m"
+echo "-------------------------------"
